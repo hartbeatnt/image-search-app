46bc29e72d9aaba8a0a410c5fb011753fb842245
Nate Hart <Hartbeatnt@gmail.com>
2022-02-09 20:33:22 -0800

    Initial Commit

---
+++ image-search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  image-search
+//
+//  Created by Nate Hart on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ image-search/image_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_searchApp.swift
+//  image-search
+//
+//  Created by Nate Hart on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct image_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
